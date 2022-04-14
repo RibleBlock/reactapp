77edc68d951ceda8e638dcab21ba3e4fe8eef00d
Riquelme <riquelmeaparecido08@gmail.com>
2022-04-14 10:26:57 -0300

    Componentes com ReactJS

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './Card.style.css';
+
+export function Card() {
+  return (
+    <div className='card'>
+      <strong>Riquelme Aparecido</strong>
+      <small>10:11:25</small>
+    </div>
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,53 @@
 import './Home.style.css';
 
+import { Card } from '../../components';
+
 export function Home() {
   return (
       <form action="POST">
         <h1>Lista de presença</h1>
         <input type="text" placeholder="Digite o nome..." />
         <button type="button">Adicionar</button>
+
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </form>
   );
 }

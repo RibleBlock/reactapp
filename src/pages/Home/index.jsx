170fc05d3bdf16ce84689224238a7890ee2b1e19
Riquelme <riquelmeaparecido08@gmail.com>
2022-04-14 15:50:05 -0300

    useState()

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
+import React, { useState } from 'react';
 import './Home.style.css';
 
 import { Card } from '../../components';
 
 export function Home() {
+  const [studentName, setStudentName] = useState('Digite alguma coisa abaixo'); // <--  [armazena, atualiza esse estado]
+  // const [clientName, setClientName] = useState(); // <--  [armazena, atualiza esse estado]
+
   return (
       <form action="POST">
-        <h1>Lista de presença</h1>
-        <input type="text" placeholder="Digite o nome..." />
+        <h1>{studentName}</h1>
+        <input
+          type="text"
+          placeholder="Digite o nome..."
+          onChange={e => setStudentName(e.target.value)}
+        />
         <button type="button">Adicionar</button>
 
         <Card user="Victor Hugo" time="07:23:12" />

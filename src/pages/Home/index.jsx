ceb32d3fb27da1db50f26dbc7913e7e34cc6d0a9
Riquelme <riquelmeaparecido08@gmail.com>
2022-04-17 12:50:00 -0300

    imutabilidade

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,23 +4,38 @@ import './Home.style.css';
 import { Card } from '../../components';
 
 export function Home() {
-  const [studentName, setStudentName] = useState('Digite alguma coisa abaixo'); // <--  [armazena, atualiza esse estado]
+  const [studentName, setStudentName] = useState(); // <--  [armazena, atualiza esse estado]
+  const [students, setStudents] = useState([]); // <-- vai armazenar os estudantes
   // const [clientName, setClientName] = useState(); // <--  [armazena, atualiza esse estado]
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+      })
+    };
+
+    setStudents(prevState => [...prevState, newStudent]);
+  }
 
   return (
       <form action="POST">
-        <h1>{studentName}</h1>
+        <h1>Lista de Presença</h1>
         <input
           type="text"
           placeholder="Digite o nome..."
           onChange={e => setStudentName(e.target.value)}
         />
-        <button type="button">Adicionar</button>
+        <button type="button" onClick={handleAddStudent}>
+          Adicionar
+        </button>
 
-        <Card user="Victor Hugo" time="07:23:12" />
-        <Card user="Pietro Ricardo" time="09:13:52" />
-        <Card user="Pietro Ricardo" time="10:43:12" />
-        <Card user="Geroge Henrique" time="14:53:32" />
+        {
+          students.map(v => <Card user={v.name} time={v.time} />)
+        }
       </form>
   );
 }
